177db2c60a49bf2c8fab7189b9db497172f0e095
abdulhafiz <xxafiz69@gmail.com>
2024-11-20 11:38:36 +0500

    add new functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,40 @@
 import './App.css';
 import SignUp from './pages/signup/SignUp.tsx';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';  // Correct import
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import Login from './pages/login/Login.tsx';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import {UserProvider} from "./UserContext.tsx";
+import { UserProvider } from './UserContext.tsx';
 import Home from './pages/home/Home.tsx';
+import Cookies from 'js-cookie';
+import { useEffect } from 'react';
 
 function App() {
     return (
         <Router>
-            <GoogleOAuthProvider clientId={"1041924221263-bvm79ds6tmdffeeg46dh7atehusbskg0.apps.googleusercontent.com"}>
+            <GoogleOAuthProvider clientId="1041924221263-bvm79ds6tmdffeeg46dh7atehusbskg0.apps.googleusercontent.com">
                 <UserProvider>
                     <Routes>
-                        {/* Route for login page */}
                         <Route path="/login" element={<Login />} />
-
-                        {/* Route for sign-up page */}
                         <Route path="/signup" element={<SignUp />} />
-
-                        {/* Default route */}
-                        <Route path="/" element={<Home />} />
+                        <Route path="/" element={<RedirectToSignUp />} />
                     </Routes>
                 </UserProvider>
             </GoogleOAuthProvider>
         </Router>
     );
+}
+
+function RedirectToSignUp() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const formData = Cookies.get('formData');
+        if (!formData) {
+            navigate('/signup');
+        }
+    }, [navigate]);
+
+    return <Home />;
 }
 
 export default App;
